6b1fdc0b9da38f37820055ccc9c1a664abf6fdf2
deepak ingale <dingale5603@gmail.com>
2023-01-16 19:41:28 +0530

    odd and even Findouts

---
+++ learning/src/learning/Simpleoddevencode.java
@@ -0,0 +1,13 @@
+package learning;
+
+public class Simpleoddevencode {
+	public static void main(String[] args) {
+		int a=121;
+		if(a % 2==0) {
+			System.out.println(a+" is even no");
+		}else
+			System.out.println(a+" is odd no");
+		
+	}
+
+}

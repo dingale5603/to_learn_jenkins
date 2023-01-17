459db8391ff38451cbf6229301c3dbe5acd49b9d
deepakYour Name <admin@192.168.1.35>
2023-01-17 13:45:21 +0530

    reverse string

--- learning/src/learning/Reverse_String.java
+++ learning/src/learning/Reverse_String.java
@@ -5,7 +5,7 @@ public class Reverse_String {
 		String name="deepak";
 		char[] eachchar=name.toCharArray();
 		for(int i=name.length()-1;i>=0;i--){
-			System.out.print(eachchar[i]);
+			System.out.print("reversed String are = "+eachchar[i]);
 		}
 	}
 
